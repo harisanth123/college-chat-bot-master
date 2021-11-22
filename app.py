d4dd6d3f176c58dd89984b906a2ac4c816164902
Harisanth P S <83811783+harisanth123@users.noreply.github.com>
2021-11-22 15:56:20 +0530

    subject

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_wtf.form import FlaskForm
 from nltk import text
 from werkzeug.utils import redirect
 from werkzeug.wrappers import response
+from wtforms.fields.numeric import IntegerField
 from chat import get_response
 from flask_wtf import FlaskForm
 from flask_login import UserMixin,login_user,LoginManager,login_required,logout_user,current_user
@@ -48,8 +49,13 @@ class RegisterForm(FlaskForm):
         existing_user_username = User.query.filter_by(
             username=username.data).first()
         if existing_user_username:
-            raise ValidationError(
-                "That user alrady exist's. please choose a different one")
+            raise ValidationError("That user alrady exist's. please choose a different one")
+
+class FeeEditForm(FlaskForm):
+    fee_type = StringField(validators=[InputRequired()])
+    value = IntegerField(validators=[InputRequired()])
+    submit =SubmitField("Edit fee")
+
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(),Length(
@@ -65,32 +71,105 @@ def index_get():
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
-    if 'fee' in text:
-        if 'hostel' in text:
-            query_result = db.engine.execute("select fee_amount from fee where fee_type = 'hostel fee'")
+
+    if 'credits'in text:
+        if 'cgm'in text:
+            query_result = db.engine.execute("select subject_credit from credits where subject_name  = 'cgm'")
             result = [ row[0] for row in query_result]
             response = result[0]
+            print(response)
 
-        elif 'college fee' in text:
-            query_result = db.engine.execute("select fee_amount from fee where fee_type = 'college fee'")
+        elif 'dsp'in text:
+            query_result = db.engine.execute("select subject_credit from credits where subject_name  = 'dsp'")
+            result = [ row[0] for row in query_result]
+            response = result[0]
+        elif 'cd'in text:
+            query_result = db.engine.execute("select subject_credit from credits where subject_name  = 'cd'")
+            result = [ row[0] for row in query_result]
+            response = result[0]
+        elif 'cn'in text:
+            query_result = db.engine.execute("select subject_credit from credits where subject_name  = 'cn'")
+            result = [ row[0] for row in query_result]
+            response = result[0]
+        elif 'hci'in text:
+            query_result = db.engine.execute("select subject_credit from credits where subject_name  = 'hci'")
+            result = [ row[0] for row in query_result]
+            response = result[0]
+        elif 'oomd'in text:
+            query_result = db.engine.execute("select subject_credit from credits where subject_name  = 'oomd'")
+            result = [ row[0] for row in query_result]
+            response = result[0]
+        elif 'dsp'in text:
+            query_result = db.engine.execute("select subject_credit from credits where subject_name  = 'dsp'")
+            result = [ row[0] for row in query_result]
+            response = result[0]
+        elif 'system lab'in text:
+            query_result = db.engine.execute("select subject_credit from credits where subject_name  = 'system lab'")
+            result = [ row[0] for row in query_result]
+            response = result[0]
+        elif 'mini project'in text:
+            query_result = db.engine.execute("select subject_credit from credits where subject_name  = 'mini project'")
+            result = [ row[0] for row in query_result]
+            response = result[0]
+        elif 'total'in text:
+            query_result = db.engine.execute("select SUM(subject_credit)from credits")
+            result = [ row[0] for row in query_result]
+            response = result[0]
+    
+        
+        
+    elif 'fee'in text:
+        
+        if 'hostel_fee'in text:
+            query_result = db.engine.execute("select fee_amount from fee where fee_type = 'hostel_fee'")
             result = [ row[0] for row in query_result]
             response = result[0]
         
-        elif 'canteen' in text:
-            query_result = db.engine.execute("select fee_amount from fee where fee_type = 'canteen fee'")
+
+        elif 'college_fee'in text:
+            query_result = db.engine.execute("select fee_amount from fee where fee_type = 'college_fee'")
             result = [ row[0] for row in query_result]
             response = result[0]
+            print(response)
+
+        
+        elif 'canteen' in text:
+            query_result = db.engine.execute("select fee_amount from fee where fee_type = 'canteen_fee'")
+            result = [ row[0] for row in query_result]
+            response = result[0]
+            print(response)
         
         elif 'union' in text:
             query_result = db.engine.execute("select fee_amount from fee where fee_type = 'union fee'")
             result = [ row[0] for row in query_result]
             response = result[0]
+        elif 'total' in text:
+            query_result = db.engine.execute("select SUM(fee_amount)from fee")
+            result = [ row[0] for row in query_result]
+            response = result[0]
             print(response)
-        else: 
-            response= get_response(text)      
+        elif 'first year fees' in text:
+            query_result = db.engine.execute("select SUM(fee_amount)from fee")
+            result = [ row[0] for row in query_result]
+            response = result[0]
+            print(response)
+    
+    elif 'subject'in text:
+        dpt_result = db.engine.execute("select * from dpt")
+        result = [ row[0] for row in dpt_result]
+        response = result[0]
+        query_result = db.engine.execute("select * from IT")
+        result = [ row[0] for row in query_result]
+        response = result[0]
+        
+
+        
+    else: 
+        response= get_response(text)      
     
     message={"answer":response}
     return jsonify(message)
+
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -118,13 +197,22 @@ def register():
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
+        
     return render_template('register.html', form=form)
 @app.route('/logout', methods=['GET','POST'])
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
-
+@app.route('/editfee', methods=['GET','POST'])
+@login_required
+def editfee():
+    form = FeeEditForm()
+    if form.validate_on_submit():
+        db.engine.execute("update fee set fee_amount="+form.value.data+"where fee_type="+form.fee_type.data)
+        return redirect(url_for('dashboard'))
+    return render_template('editfee.html',form=form)
+    
 
 if __name__=="__main__":
-    app.run(debug=True)
+   app.run(debug=True)
