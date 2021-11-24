cb673a3920c291f00d8a02dc32e4ec4a44b1cdf0
Harisanth P S <83811783+harisanth123@users.noreply.github.com>
2021-11-25 00:00:53 +0530

    dpt pinting

--- app.py
+++ app.py
@@ -155,14 +155,11 @@ def predict():
             print(response)
     
     elif 'subject'in text:
-        dpt_result = db.engine.execute("select dpt_name from dpt")
+        dpt_result = db.engine.execute("select dpt_code from dpt")
         result = [ row[0] for row in dpt_result]
-        response = result[0]
-        text2 = request.get_json().get("message")
-        if 'it' in text2:
-            it_result = db.engine.execute("select sub from IT")
-            result = [ row[0] for row in it_result]
-            response = result[0]
+        response = result
+        
+        
 
 
         
